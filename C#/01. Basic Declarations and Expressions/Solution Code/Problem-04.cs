3ab6844f632d2fa66d77cbcbb3bb130a87462911
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-25 14:37:14 +0600

    Create Problem-04.cs

---
+++ C#/01. Basic Declarations and Expressions/Solution Code/Problem-04.cs
@@ -0,0 +1,20 @@
+//C# Solution:
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int result_1 = -1 + 4 * 6;
+        Console.WriteLine(result_1);
+
+        int result_2 = (35 + 5) % 7;
+        Console.WriteLine(result_2);
+
+        int result_3 = 14 + (-4) * 6 / 11;
+        Console.WriteLine(result_3);
+
+        int result_4 = 2 + 15 / 6 * 1 - 7 % 2;
+        Console.WriteLine(result_4);
+    }
+}
