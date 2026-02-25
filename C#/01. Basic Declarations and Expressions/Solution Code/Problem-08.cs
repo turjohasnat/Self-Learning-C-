a03ebc01cb9981a21a78a08f29f4b33d18b431d6
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-25 15:14:05 +0600

    Create Problem-08.cs

---
+++ C#/01. Basic Declarations and Expressions/Solution Code/Problem-08.cs
@@ -0,0 +1,45 @@
+//C# Solution:
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int x;
+        Console.Write("Enter the number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        int result;
+        
+        result = x * 1;
+        Console.WriteLine("{0} x {1} = {2}", x, 1, result);
+        
+        result = x * 2;
+        Console.WriteLine("{0} x {1} = {2}", x, 2, result);
+        
+        result = x * 3;
+        Console.WriteLine("{0} x {1} = {2}", x, 3, result);
+        
+        result = x * 4;
+        Console.WriteLine("{0} x {1} = {2}", x, 4, result);
+        
+        result = x * 5;
+        Console.WriteLine("{0} x {1} = {2}", x, 5, result);
+        
+        result = x * 6;
+        Console.WriteLine("{0} x {1} = {2}", x, 6, result);
+        
+        result = x * 7;
+        Console.WriteLine("{0} x {1} = {2}", x, 7, result);
+        
+        result = x * 8;
+        Console.WriteLine("{0} x {1} = {2}", x, 8, result);
+        
+        result = x * 9;
+        Console.WriteLine("{0} x {1} = {2}", x, 9, result);
+        
+        result = x * 10;
+        Console.WriteLine("{0} x {1} = {2}", x, 10, result);
+        
+    }
+}
