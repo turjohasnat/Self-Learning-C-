c7bc458b1b38228d5e545054eb78b0c84c5e075a
Md. Turjo Hasnat <149037574+turjohasnat@users.noreply.github.com>
2026-02-25 14:29:10 +0600

    Create Problem-02.cs

---
+++ C#/01. Basic Declarations and Expressions/Solution Code/Problem-02.cs
@@ -0,0 +1,15 @@
+//C# Solution:
+using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int num1, num2;
+
+        num1 = int.Parse(Console.ReadLine());
+        num2 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(num1 + num2);
+    }
+}
